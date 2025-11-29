670c4234d79643953bfff9f967adee3434d6b14c
dariomartinovski <master15820@gmail.com>
2025-11-29 12:05:35 +0100

    Changed the styling, so the map isn't full screen in order to make space for the toolbar section, which will be the search bar and view switch,

---
+++ home-radar-frontend/src/app/shared/components/property-details-popup/property-details-popup.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Property } from '../../../interfaces/property.interface';
+
+@Component({
+  selector: 'property-details-popup',
+  templateUrl: './property-details-popup.component.html',
+  styleUrls: ['./property-details-popup.component.scss']
+})
+export class PropertyDetailsPopupComponent {
+  @Input() property?: Property;
+  @Output() closePopup = new EventEmitter<void>();
+
+  close() {
+    this.closePopup.emit();
+  }
+
+  getImageUrl(property: any): string {
+    return property.imageUrl
+      ? property.imageUrl
+      : (property.type === 'HOUSE'
+        ? 'assets/images/sale_house_small.jpg'
+        : 'assets/images/sale_flat_small.jpg');
+  }
+}
